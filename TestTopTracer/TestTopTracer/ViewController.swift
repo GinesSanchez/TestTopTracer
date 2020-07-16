8c87c22b81a8d35b3049fd92a93ae5cb57eb1198
ginessan <ginesinho@gmail.com>
2020-07-16 09:25:21 +0200

    Add empty single view project.

---
+++ TestTopTracer/TestTopTracer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTopTracer
+//
+//  Created by Gines Sanchez Merono on 2020-07-16.
+//  Copyright © 2020 Ginés Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestTopTracer/TestTopTracerTests/TestTopTracerTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestTopTracerTests.swift
+//  TestTopTracerTests
+//
+//  Created by Gines Sanchez Merono on 2020-07-16.
+//  Copyright © 2020 Ginés Sanchez. All rights reserved.
+//
+
+import XCTest
+@testable import TestTopTracer
+
+class TestTopTracerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
